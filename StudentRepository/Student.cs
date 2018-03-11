d47b9698a63c4a4fef5dc01975d7d6c2ca6062c0
mopak <dobromirstefanov96@gmail.com>
2018-03-11 21:00:45 +0200

    Created form template

--- StudentRepository/Student.cs
+++ StudentRepository/Student.cs
@@ -7,12 +7,14 @@ namespace StudentRepository
         public string firstName;
         public string secondName;
         public string lastName;
+
         public string facility;
         public string specialization;
         public EducationDegree educationDegree;
         public DegreeStatus status;
         public string facNumber;
         public DateTime lastCheck;
+
         public ushort course;
         public ushort stream;
         public char group;
