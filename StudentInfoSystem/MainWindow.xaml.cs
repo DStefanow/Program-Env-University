891e2190e3cdfc4ea6c057a69df59a81810857a1
mopak <dobromirstefanov96@gmail.com>
2018-03-15 09:59:04 +0200

    Added enable/disable text boxes/combo boxes functionality

--- StudentInfoSystem/MainWindow.xaml.cs
+++ StudentInfoSystem/MainWindow.xaml.cs
@@ -48,5 +48,41 @@ namespace StudentInfoSystem
             streamBox.Clear();
             groupBox.Clear();
         }
+
+        private void btnMakeDisable_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear personal info
+            firstNameBox.IsEnabled = false;
+            secondNameBox.IsEnabled = false;
+            lastNameBox.IsEnabled = false;
+
+            // Clear student info
+            educationDegreeBox.IsEnabled = false;
+            degreeStatusBox.IsEnabled = false;
+            courseBox.IsEnabled = false;
+            facilityBox.IsEnabled = false;
+            specializationBox.IsEnabled = false;
+            facNumberBox.IsEnabled = false;
+            streamBox.IsEnabled = false;
+            groupBox.IsEnabled = false;
+        }
+
+        private void btnMakeEnable_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear personal info
+            firstNameBox.IsEnabled = true;
+            secondNameBox.IsEnabled = true;
+            lastNameBox.IsEnabled = true;
+
+            // Clear student info
+            educationDegreeBox.IsEnabled = true;
+            degreeStatusBox.IsEnabled = true;
+            courseBox.IsEnabled = true;
+            facilityBox.IsEnabled = true;
+            specializationBox.IsEnabled = true;
+            facNumberBox.IsEnabled = true;
+            streamBox.IsEnabled = true;
+            groupBox.IsEnabled = true;
+        }
     }
 }
