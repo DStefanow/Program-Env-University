c69b835d656241c1a1c99915f1ba890badd7891e
mopak <dobromirstefanov96@gmail.com>
2018-05-11 14:44:54 +0300

    Added Observable Collection for test

--- EasyMVVM/Model.cs
+++ EasyMVVM/Model.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace EasyMVVM
 {
     class Model
     {
+        ObservableCollection<string> _data = new ObservableCollection<string>();
+
+        public ObservableCollection<string> GetData()
+        {
+            _data.Add("First Entry");
+            _data.Add("Second Entry");
+            _data.Add("Third Entry");
+
+            return _data;
+        }
     }
 }
