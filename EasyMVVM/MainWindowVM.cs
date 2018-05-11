f02168a82d597f125e3f4badab2511540803df05
mopak <dobromirstefanov96@gmail.com>
2018-05-11 14:55:19 +0300

    Created MainWindows class with base properties

---
+++ EasyMVVM/MainWindowVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace EasyMVVM
+{
+    class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<string> _BackingProperty;
+
+        public MainWindowVM()
+        {
+            Model model = new Model();
+            BoundProperty = model.GetData();
+        }
+
+        public ObservableCollection<string> BoundProperty
+        {
+            get { return _BackingProperty; }
+            set {
+                _BackingProperty = value;
+                PropChanged("Bound Property");
+            }
+        }
+
+        public void PropChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
