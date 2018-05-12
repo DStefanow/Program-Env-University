c22881b3e15abf3f4521753108a1e2eaabbe3975
mopak <dobromirstefanov96@gmail.com>
2018-05-12 15:49:52 +0300

    Added interface methods

--- WpfExample/RelayCommand.cs
+++ WpfExample/RelayCommand.cs
@@ -5,6 +5,16 @@ namespace WpfExample
 {
     class RelayCommand : ICommand
     {
+        public event EventHandler CanExecuteChanged;
 
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
