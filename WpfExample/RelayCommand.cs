5fc625b776fb20df4f64519e7cb5ba0af4e5d217
mopak <dobromirstefanov96@gmail.com>
2018-05-12 16:00:44 +0300

    Added required methods

--- WpfExample/RelayCommand.cs
+++ WpfExample/RelayCommand.cs
@@ -3,18 +3,77 @@ using System.Windows.Input;
 
 namespace WpfExample
 {
-    class RelayCommand : ICommand
+    public class RelayCommand : ICommand
     {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
         public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute) : this(execute, DefaultCanExecute)
+        {
+
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            if (canExecute == null)
+            {
+                throw new ArgumentNullException("canExecute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteHandler
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                this.CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                this.CanExecuteChangedInternal -= value;
+            }
+        }
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return this.canExecute != null && this.canExecute(parameter);
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            this.execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChangedInternal;
+
+            if (handler != null)
+            {
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Destroy()
+        {
+            this.canExecute = _ => false;
+            this.execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
         }
     }
 }
