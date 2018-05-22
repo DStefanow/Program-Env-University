20f8aab9dfcd5fc89cfc019baa5480c5c0619425
mopak <dobromirstefanov96@gmail.com>
2018-05-22 23:56:50 +0300

    Added connection string to UserLogin and StudentRepository

--- UserLogin/Properties/Settings.Designer.cs
+++ UserLogin/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace StudentRepository.Properties {
+namespace UserLogin.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
