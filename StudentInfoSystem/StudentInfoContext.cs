4d5346e87273a0d0b894a89e733010df32e01ab9
mopak <dobromirstefanov96@gmail.com>
2018-05-23 00:25:28 +0300

    Added EF to projects

--- StudentInfoSystem/StudentInfoContext.cs
+++ StudentInfoSystem/StudentInfoContext.cs
@@ -1,30 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.SqlClient;
-using System.Linq;
 
 namespace StudentInfoSystem
 {
     class StudentInfoContext : DbContext
     {
-        public DbSet<StudentInternships> Internships { get; set; }
 
-        public StudentInfoContext() : base("StudentInfo")
-        {
-            Properties.Settings.Default.DbSettings =
-                "Data Source=(local);Initial Catalog=StudentInfo;Integrated Security=SSPI";
-
-            SqlConnection conn = new SqlConnection(Properties.Settings.Default.DbSettings);
-           
-            conn.Open();
-
-        }
-
-
-        public IQueryable<StudentInternships> getAllStudents()
-        {
-            return Set<StudentInternships>();
-        }
     }
 }
