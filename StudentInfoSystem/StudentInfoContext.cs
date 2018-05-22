cc1826a8c0ae4a52b1e8fe0c6995af7f056e9a59
mopak <dobromirstefanov96@gmail.com>
2018-05-23 00:29:39 +0300

    Created class that make the connection to database

--- StudentInfoSystem/StudentInfoContext.cs
+++ StudentInfoSystem/StudentInfoContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using StudentRepository;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using UserLogin;
 
 namespace StudentInfoSystem
 {
     class StudentInfoContext : DbContext
     {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<User> Users { get; set; }
 
+        public StudentInfoContext() : base(Properties.Settings.Default.DbConnect)
+        {
+
+        }
     }
 }
